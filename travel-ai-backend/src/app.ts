8642fa9a7e03b8663b090ca2ee5d65934850c407
Abhishek N <abhishekpes123@gmail.com>
2026-05-28 12:56:38 +0530

    first commit

--- travel-ai-backend/src/app.ts
+++ travel-ai-backend/src/app.ts
@@ -10,9 +10,18 @@ import itineraryRoutes from "./routes/itinerary.routes.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 import { apiLimiter } from "./middleware/rateLimit.middleware.js";
 import { UPLOAD_DIR } from "./config/paths.js";
+
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://booking-travel-mern.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(helmet());
 
@@ -20,8 +29,8 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
-
 app.use("/api", apiLimiter);
+
 app.use("/uploads", express.static(UPLOAD_DIR));
 
 app.use("/api/v1/auth", authRoutes);
